763d5eba6ab0545f6f986162520a01028dcd1094
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-11-19 13:36:36 +0300

    salomlar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <div className=''>
-       <ul className=''>fabwFAFGb</ul>
+       <ul className=''>
+        
+       </ul>
     </div>
   )
 }

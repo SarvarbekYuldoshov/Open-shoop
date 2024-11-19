7ca27e3e10825b734f8238705949900056aa16eb
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-11-19 17:51:15 +0300

    salomlar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,13 @@ import Rasm_1 from "../images/Logo.png"
 const Header = () => {
   return (
     <div className='header'>
+      <div className='header-container'>
        <img src={Rasm_1} alt="Logo qismi bu qism" />
+           <ul className='header-list'>
+              <li className=''><input className='' type="text" /></li>
+              <li className=''><input className='' type="text" /></li>
+           </ul>
+           </div>
     </div>
   )
 }

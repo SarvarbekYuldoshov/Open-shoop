d7c0dfc157ea291ab3ad79e0298fdf741fd6e576
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-11-19 16:06:00 +0300

    salomlar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import Rasm_1 from "../images/Logo.png"
 const Header = () => {
   return (
     <div className=''>
+      <img className='' src={Rasm_1} alt="" />
        <ul className=''>
         
        </ul>

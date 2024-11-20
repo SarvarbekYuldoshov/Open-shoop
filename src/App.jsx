ebbfcfd7e7879a167536daf8c3f9615288ed09d3
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-11-20 18:25:21 +0300

    salomlar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import './index.css'
 import Header from './Components/Header/Header'
+import Layout from './Components/Layout/Layout'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div className='App'>
+         <Layout/>
          <Header/>
       </div>
     </>

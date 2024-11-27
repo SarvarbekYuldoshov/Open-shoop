01d6735db9756a0559d46fd31fbc12ce2053d290
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-11-27 17:27:33 +0300

    salomlar

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -5,7 +5,11 @@ const Layout = () => {
     <div className='Layout'>
        <div className='Layout-container'>
           <ul className='Layout-list'>
-
+             <select className="select" onChange={handleLanguageChange} value={i18n.language}>
+                <option value="uz">Uzbek</option>
+                <option value="en">English</option>
+                <option value="ru">Russian</option>
+              </select>
           </ul>
           <ul className='Layout-list'>
 

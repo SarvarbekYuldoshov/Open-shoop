7dadafaaccde1fdabedb9475e13962d3d97ab8c3
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-11-27 15:48:48 +0300

    salomlar

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -3,7 +3,7 @@ import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import uzTranslation from "../src/Tillar/uzbek/uzbek.json"
+import uzTranslation from "../src/Components/Languages/uzbek.json"
 import enTranslation from "../src/Components/Languages/english.json"
 import enTranslation from "../src/Components/Languages/russian.json"
 

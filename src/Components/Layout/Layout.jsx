1c06b0fb49d26fff22e7daaffa038b91fa8a927f
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-11-27 17:34:17 +0300

    salomlar

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const Layout = () => {
+  const { t, i18n } = useTranslation();
+
+  const handleLanguageChange = (e) => {
+    i18n.changeLanguage(e.target.value);
+  };
   return (
     <div className='Layout'>
        <div className='Layout-container'>

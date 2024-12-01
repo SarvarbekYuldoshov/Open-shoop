9f05e18606e918e80d1f656465e9e179b3d7fc81
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-12-01 17:56:44 +0300

    salomllar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,11 @@ const Navbar = () => {
        <div className='Navbar-container'>
            <ul className='Navbar-list'>
               <li className='Navbar-item'><a className='Navbar-link' href=""></a></li>
+              <li className='Navbar-item'><a className='Navbar-link' href=""></a></li>
+              <li className='Navbar-item'><a className='Navbar-link' href=""></a></li>
+              <li className='Navbar-item'><a className='Navbar-link' href=""></a></li>
+              <li className='Navbar-item'><a className='Navbar-link' href=""></a></li>
+              <li className='Navbar-item'><a className='Navbar-link' href=""></a></li>
            </ul>
        </div>
     </div>

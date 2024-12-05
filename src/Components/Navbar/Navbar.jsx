5952c5d5d3c045e6402a4b19a08a3279e81d7b8e
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-12-05 11:57:51 +0300

    sslaodhdjs

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,14 +4,16 @@ const Navbar = () => {
   return (
     <div className='Navbar'>
        <div className='Navbar-container'>
-           <ul className='Navbar-list'>
-              <li className='Navbar-item'><a className='Navbar-link' href="">Cell Phones</a></li>
-              <li className='Navbar-item'><a className='Navbar-link' href="">Computers&Laptop</a></li>
-              <li className='Navbar-item'><a className='Navbar-link' href="">Tv&Video </a></li>
-              <li className='Navbar-item'><a className='Navbar-link' href="">Asseccories</a></li>
-              <li className='Navbar-item'><a className='Navbar-link' href="">Home appliance</a></li>
-              <li className='Navbar-item'><a className='Navbar-link' href="">Video Games&Consoles</a></li>
-           </ul>
+           <div className='Navbar-mindle'>
+                <ul className='Navbar-list'>
+                    <li className='Navbar-item'><a className='Navbar-link' href="">Cell Phones</a></li>
+                    <li className='Navbar-item'><a className='Navbar-link' href="">Computers&Laptop</a></li>
+                    <li className='Navbar-item'><a className='Navbar-link' href="">Tv&Video </a></li>
+                    <li className='Navbar-item'><a className='Navbar-link' href="">Asseccories</a></li>
+                    <li className='Navbar-item'><a className='Navbar-link' href="">Home appliance</a></li>
+                    <li className='Navbar-item'><a className='Navbar-link' href="">Video Games&Consoles</a></li>
+                </ul>
+           </div>
        </div> 
     </div>
   )

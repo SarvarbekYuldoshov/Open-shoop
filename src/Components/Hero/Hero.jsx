45c9d44c536a77ec5585b5edaf7d97767d2308a0
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-12-05 16:52:15 +0300

    salomlar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./Hero.css"
+import Rasm_6 from "../images/rasm6.png"
+import Rasm_7 from "../images/rasm7.png"
 const Hero = () => {
   return (
-    <div className=''>
-
+    <div className='hero'>
+       <div className='hero-container'>
+           <ul className='hero-list'>
+               <img className='hero-mg' src={Rasm_6} alt="" />
+           </ul>
+           <ul className='hero-list'>
+            
+           </ul>
+       </div>
     </div>
   )
 }

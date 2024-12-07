ddfca11a1921e5692ced84b6a8d526aedd4e35a2
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-12-07 14:36:23 +0300

    salomplar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,16 +2,27 @@ import React from 'react'
 import "./Hero.css"
 import Rasm_6 from "../images/rasm6.png"
 import Rasm_7 from "../images/rasm7.png"
+import Rasm_8 from "../images/rasm8.png"
 const Hero = () => {
   return (
     <div className='hero'>
        <div className='hero-container'>
            <ul className='hero-list'>
-               <img className='hero-mg' src={Rasm_6} alt="" />
+               <img className='hero-img' src={Rasm_6} alt="" />
            </ul>
-           <ul className='hero-list'>
-            
-           </ul>
+           <div className='hero-mendle'>
+                <ul className='hero-list'>
+                    <img className='hero-img-1' src={Rasm_7} alt="" />
+                </ul>
+                <ul className='hero-list'>
+                    <li className=''>
+                        <img className='hero-img-2' src={Rasm_8} alt="" /> 
+                    </li>
+                    <li className=''>
+                        <img className='hero-img-3' src={Rasm_6} alt="" />
+                    </li>
+                </ul>
+           </div>
        </div>
     </div>
   )

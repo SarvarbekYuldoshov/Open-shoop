00a8a475777aefcb1d916002823c415f18410ced
sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-12-10 16:07:40 +0300

    salomlar

--- src/Components/CellPhone/CellPhone.jsx
+++ src/Components/CellPhone/CellPhone.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Button } from 'antd';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import Rasm_4 from "../images/rasm4.png";
+import Rasm_5 from "../images/rasm5.png";
+import './Navbar.css';
 
-const CellPhone = () => {
+const SwiperSection = ({ showModal }) => {
   return (
-    <div className='CellPhone'>
-        <div className='CellPhone-container'>
+    <Swiper
+      pagination={{
+        dynamicBullets: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      {[...Array(2)].map((_, index) => (
+        <React.Fragment key={index}>
+          <SwiperSlide>
+            <div className='navbar-mendle'>
+              <ul className='navbar-list'>
+                <img className='navbar-img' src={Rasm_4} alt="" />
+              </ul>
+              <ul className='navbar-list'>
+                <li>
+                  <h1 className='navbar-title'>Color your days with small joy</h1>
+                </li>
+                <li>
+                  <Button className='navbar-btn' onClick={showModal}>SHOP NOW</Button>
+                </li>
+              </ul>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='navbar-mendle'>
+              <ul className='navbar-list'>
+                <img className='navbar-img' src={Rasm_5} alt="" />
+              </ul>
+              <ul className='navbar-list'>
+                <li>
+                  <h1 className='navbar-title'>Color your days with a little joy watch</h1>
+                </li>
+                <li>
+                  <Button className='navbar-btn' onClick={showModal}>SHOP NOW</Button>
+                </li>
+              </ul>
+            </div>
+          </SwiperSlide>
+        </React.Fragment>
+      ))}
+    </Swiper>
+  );
+};
 
-        </div>
-    </div>
-  )
-}
-
-export default CellPhone
+export default SwiperSection;
